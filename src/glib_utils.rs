5ef432a4205d925cf53b2a4646e22688582b5b05
Martin Robinson <mrobinson@igalia.com>
2018-04-19 12:48:58 +0200

    Improve snake casing of class names
    Hopefully this will make simple snake case bugs hisstory.
    
    Fixes #23.

--- src/glib_utils.rs
+++ src/glib_utils.rs
@@ -52,9 +52,36 @@ pub fn glib_callback_guard() -> Tokens {
 }
 
 pub fn lower_case_instance_name(instance_name: &str) -> String {
-    let mut name_chars = instance_name.chars();
-    let first_char: char = name_chars.next().unwrap();
-    first_char.to_lowercase().chain(name_chars).collect()
+    let mut char_iterator = instance_name.char_indices();
+    let mut start_index = match char_iterator.next() {
+        Some((index, _)) => index,
+        None => return String::new(),
+    };
+
+    let mut parts = Vec::new();
+    'outer: loop {
+        // If the next bit starts with a sequence of uppercase characters, we include them
+        // all. This is done in order to have a better default behavior with class names that
+        // contains acronyms. See the `lower_cases_with_sequential_uppercase_characters` test
+        // below.
+        let mut found_non_uppercase_character = false;
+
+        for (end_index, character) in &mut char_iterator {
+            let character_is_uppercase = character.is_uppercase();
+            if found_non_uppercase_character && character_is_uppercase {
+                parts.push(instance_name[start_index..end_index].to_lowercase());
+                start_index = end_index;
+                continue 'outer;
+            } else {
+                found_non_uppercase_character |= !character_is_uppercase;
+            }
+        }
+
+        parts.push(instance_name[start_index..].to_lowercase());
+        break 'outer;
+    }
+
+    parts.join("_")
 }
 
 pub mod tests {
@@ -62,9 +89,37 @@ pub mod tests {
 
     pub fn run() {
         lower_cases_simple_names();
+        lower_cases_non_ascii_names();
+        lower_cases_with_sequential_uppercase_characters();
     }
 
     fn lower_cases_simple_names() {
         assert_eq!("foo", lower_case_instance_name("Foo"));
+        assert_eq!(
+            "snake_case_sliding_through_the_grass",
+            lower_case_instance_name("SnakeCaseSlidingThroughTheGrass")
+        );
+        assert_eq!("", lower_case_instance_name(""));
+        assert_eq!("ifyoureallywantto", lower_case_instance_name("ifyoureallywantto"));
+        assert_eq!("if_you_really_want_to", lower_case_instance_name("if_you_really_want_to"));
+    }
+
+    fn lower_cases_non_ascii_names() {
+        assert_eq!("y̆es", lower_case_instance_name("Y̆es"));
+        assert_eq!("trying_this_y̆es_y̆es", lower_case_instance_name("TryingThisY̆esY̆es"));
+        assert_eq!("y̆es_y̆es_trying_this", lower_case_instance_name("Y̆esY̆esTryingThis"));
+    }
+
+    fn lower_cases_with_sequential_uppercase_characters() {
+        assert_eq!("gtk_rbtree", lower_case_instance_name("GtkRBTree"));
+        assert_eq!("rbtree_internals", lower_case_instance_name("RBTreeInternals"));
+
+        // This may or may not be what we want, but for now this is the behavior that we expect.
+        assert_eq!("gtkrbtree", lower_case_instance_name("GTKRBTree"));
+
+        assert_eq!(
+            "thisisaterribleclassname",
+            lower_case_instance_name("THISISATERRIBLECLASSNAME")
+        );
     }
 }
