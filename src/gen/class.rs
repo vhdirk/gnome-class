dae6e6f99e0f40075d026edc7715ebfa37d17a93
Federico Mena Quintero <federico@gnome.org>
2018-04-20 16:40:29 +0200

    Simplify the initialization of ClassContext

--- src/gen/class.rs
+++ src/gen/class.rs
@@ -34,8 +34,6 @@ impl<'ast> ClassContext<'ast> {
         // commonly-used symbol names for the class in question, for
         // example, "FooClass" out of "Foo".
 
-        let InstanceName = &class.name;
-
         // If our instance name is "Foo" and we have a suffix "Bar", generates a
         // "FooBar" Ident.  These are used for the generated module name,
         // instance/class struct names, etc.
@@ -48,42 +46,27 @@ impl<'ast> ClassContext<'ast> {
         // `syn` ideally, but it's not clear how that would change, if at all
         let container_name = |suffix: &str| {
             Ident::new(
-                &format!("{}{}", InstanceName.as_ref(), suffix),
+                &format!("{}{}", class.name.as_ref(), suffix),
                 Span::call_site(),
             )
         };
 
-        let InstanceNameFfi = container_name("Ffi");
-        let PrivateStructName = container_name("Priv");
-        let ModuleName = container_name("Mod"); // toplevel "InstanceMod" module name
-        let ClassName = container_name("Class");
-        let PrivateClassName = container_name("ClassPrivate");
-        let InstanceExt = container_name("Ext"); // public trait with all the class's methods
-
-        let GObject = tokens_GObject();
-        let GObjectFfi = tokens_GObjectFfi();
-        let GObjectClassFfi = tokens_GObjectClassFfi();
-
-        let ParentInstance = &class.parent;
-        let ParentInstanceFfi = &class.parent_ffi;
-        let ParentClassFfi = &class.parent_class_ffi;
-
         ClassContext {
             program,
             class,
-            PrivateStructName,
-            ModuleName,
-            InstanceName,
-            ClassName,
-            PrivateClassName,
-            ParentInstance,
-            ParentInstanceFfi,
-            ParentClassFfi,
-            GObject,
-            GObjectFfi,
-            GObjectClassFfi,
-            InstanceExt,
-            InstanceNameFfi,
+            PrivateStructName: container_name("Priv"),
+            ModuleName: container_name("Mod"), // toplevel "InstanceMod" module name
+            InstanceName: &class.name,
+            ClassName: container_name("Class"),
+            PrivateClassName: container_name("ClassPrivate"),
+            ParentInstance: &class.parent,
+            ParentInstanceFfi: &class.parent_ffi,
+            ParentClassFfi: &class.parent_class_ffi,
+            GObject: tokens_GObject(),
+            GObjectFfi: tokens_GObjectFfi(),
+            GObjectClassFfi: tokens_GObjectClassFfi(),
+            InstanceExt: container_name("Ext"), // public trait with all the class's methods
+            InstanceNameFfi: container_name("Ffi"),
         }
     }
 
